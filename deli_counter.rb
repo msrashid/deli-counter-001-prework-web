bcf43cc8a768a033a8bba0a277e2b0c1b45c6d1e
msrashid <wlitmilkyway@gmail.com>
2016-05-01 16:27:49 +0000

    nitrous-backup at Sun May  1 16:27:48 UTC 2016

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,20 @@
-# Write your code here.
+def line (one)
+  y=one.length
+  if (y=0)
+ puts "The line is currently empty."
+  else
+  puts "The line is currently: "
+  x=0
+  while (x<y)
+  puts "#{x+1}. #{one[0]}."
+  x=x+1
+  end
+  end
+end
+
+def take_a_number (one, two)
+end
+
+def now_serving (one)
+end
+
